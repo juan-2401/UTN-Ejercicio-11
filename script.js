b5eb2e43c07622ba3b112af25569392d334b2a3c
juan-2401 <juan-2401@hotmail.com>
2021-05-11 20:16:44 -0300

    exercise complete

--- script.js
+++ script.js
@@ -2,10 +2,61 @@ console.log("JS funcionando");
 
 var clasificaciones = ["Ana", "Pedro", "Mariano", "Clara", "Axel", "Anton"];
 
-let posicion = 1;
+/* for clasico */
 
-document.write("Clasificación Actual: " + "<br>");
+document.write("<br>" + "Clasificación 1: " + "<br>");
+
+let posicion1 = 1;
 
 for (let i = 0; i < clasificaciones.length; i++) {
-    document.write(`${posicion++}° - ${clasificaciones[i]} <br>`);
+    document.write(`${posicion1++}° - ${clasificaciones[i]} <br>`);
 };
+
+/* for clasico */
+
+document.write("<br>" + "Clasificación 2: " + "<br>");
+
+clasificaciones[2] = "Clara";
+clasificaciones[3] = "Mariano";
+
+let posicion2 = 1;
+
+for (let i = 0; i < clasificaciones.length; i++) {
+    document.write(`${posicion2++}° - ${clasificaciones[i]} <br>`);
+};
+
+/* for ciclo */
+
+document.write("<br>" + "Clasificación 3: " + "<br>");
+
+clasificaciones.pop();
+
+let posicion3 = 1
+
+for (const ciclo of clasificaciones) {
+    document.write(`${posicion3++}° - ${ciclo} <br>`);      
+}
+
+/* foreach */
+
+document.write("<br>" + "Clasificación 4: " + "<br>");
+
+clasificaciones.splice(1, 0, "Roberto", "Analia")
+
+let posicion4 = 1
+
+clasificaciones.forEach(element => {
+    document.write(`${posicion4++}° - ${element} <br>`);   
+});
+
+/* foreach */
+
+document.write("<br>" + "Clasificación 5: " + "<br>");
+
+clasificaciones.unshift("Marcela");
+
+let posicion5 = 1
+
+clasificaciones.forEach(element => {
+    document.write(`${posicion5++}° - ${element} <br>`);   
+});
